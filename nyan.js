75613b42ef9ead7e7fab8160f4b724a6359c8b04
Kris de Rycke <derycke.kris@gmail.com>
2019-05-28 14:44:37 +0200

    set animation function

---
+++ nyan.js
@@ -0,0 +1,18 @@
+document.getElementById("start").addEventListener("click", rain);
+
+function rain() {
+  var elem = document.getElementById("nyan");
+  var pos = 0;
+  var id = setInterval(frame, 40);
+  elem.style.display = "block";
+
+  function frame() {
+    if (pos == 100) {
+      clearInterval(id);
+    } else {
+      pos++;
+      elem.style.top = pos + "%"; //writes 100% in "pos" in line 5; giving it a value
+      elem.style.left = pos + "%";
+    }
+  }
+}
